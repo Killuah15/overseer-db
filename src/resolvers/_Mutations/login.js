f09b28aa064b796c0bac745bf09fc2cd08a5c147
Killuah15 <Rockingrob@web.de>
2019-01-08 23:13:35 +0100

    added email validator
    added email validator because i don't know any regex

--- src/resolvers/_Mutations/login.js
+++ src/resolvers/_Mutations/login.js
@@ -3,19 +3,17 @@ import {
     passwordMatch,
   } from '../../utils/Cryptic'
 
-const login = async (parent, { data }, { prisma, request }, info) => {
+const login = async (parent, { data }, { prisma, response }, info) => {
     //fetch user (also looks at if the user exists)
     const user = await prisma.query.user({
       where: {
         email: data.email
       }
     })
-
     if (!user) throw new Error('Unable to login')
 
     //compare DB saved hashed password with input password
     const isMatch = await passwordMatch(data.password, user.password)
-
     if (!isMatch) throw new Error('Unable to login')
 
     const token = generateToken(user.id)
--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,8 +1,12 @@
+import EmailValidator from 'email-validator';
+
 //See if password has a length between [8,16] chars
 const isValidPassword = password => password.length >= 8 && password.length <= 16
 
 //See if Email has the @ Symbol inside
-const isValidEmail = email => email.includes('@')
+const isValidEmail = email => {
+    return EmailValidator.validate(email)
+}
 
 const validateArg = arg => arg ? arg : null 
 
