fc8aab97a0177d186fe132b6d535d92fc7e63a59
Killuah15 <Rockingrob@web.de>
2019-01-09 17:10:19 +0100

    schema evolution
    defined conditions further

--- src/resolvers/_Mutations/Create/createCreature.js
+++ src/resolvers/_Mutations/Create/createCreature.js
@@ -1,76 +1,86 @@
-import {
-    getUserID
-  } from '../../../utils/Cryptic'
+import { getUserID } from '../../../utils/Cryptic'
 
 const createCreature = async (parent, { data }, { prisma, request }, info) => {
-    //get UserID stored in sessionStorage (through jwt)
-    const uid = await getUserID(request)
+  //get UserID stored in sessionStorage (through jwt)
+  const uid = await getUserID(request)
 
-    //destructure conditions input
-    const {
-      toughness,
-      painThreshold,
-      corruption,
-      corruptionThreshold,
-      corruptionPermanent
-    } = data.Conditions
+  //destructure conditions input
+  const { toughness, painThreshold } = data.Conditions.fitness
 
-    //destructure attributes input
-    const {
-      accurate,
-      cunning,
-      discreet,
-      persuasive,
-      quick,
-      resolute,
-      strong,
-      vigilant,
-      defense
-    } = data.attributes
+  const { current, threshold, permanent } = data.Conditions.corruption
 
-    //create Creature on the database - map inputs to DB graphql Mutation
-    const creature = await prisma.mutation.createCreature(
-      {
-        data: {
-          ...data,
-          author: {
-            connect: {
-              id: uid
-            }
-          },
-          event: {
-            connect: {
-              id: data.event
-            }
-          },
-          Conditions: {
-            create: {
-              toughness,
-              painThreshold,
-              corruption,
-              corruptionThreshold,
-              corruptionPermanent
-            }
-          },
-          attributes: {
-            create: {
-              accurate,
-              cunning,
-              discreet,
-              persuasive,
-              quick,
-              resolute,
-              strong,
-              vigilant,
-              defense
+  //destructure attributes input
+  const {
+    accurate,
+    cunning,
+    discreet,
+    persuasive,
+    quick,
+    resolute,
+    strong,
+    vigilant,
+    defense
+  } = data.attributes
+
+  //create Creature on the database - map inputs to DB graphql Mutation
+  const creature = await prisma.mutation.createCreature(
+    {
+      data: {
+        ...data,
+        author: {
+          connect: {
+            id: uid
+          }
+        },
+        event: {
+          connect: {
+            id: data.event
+          }
+        },
+        Conditions: {
+          create: {
+            physicalConditions: {
+              create: {
+                fitnessCondition: {
+                  create: {
+                    toughness,
+                    painThreshold
+                  }
+                }
+              }
+            },
+            spiritualConditions: {
+              create: {
+                corruptionCondition: {
+                  create: {
+                    current,
+                    threshold,
+                    permanent
+                  }
+                }
+              }
             }
           }
+        },
+        attributes: {
+          create: {
+            accurate,
+            cunning,
+            discreet,
+            persuasive,
+            quick,
+            resolute,
+            strong,
+            vigilant,
+            defense
+          }
         }
-      },
-      info
-    )
+      }
+    },
+    info
+  )
 
-    return creature
-  }
+  return creature
+}
 
-  export default createCreature
+export default createCreature
--- src/resolvers/_Mutations/Update/updateCreature.js
+++ src/resolvers/_Mutations/Update/updateCreature.js
@@ -1,35 +1,59 @@
-import { getUserID } from '../../../utils/Cryptic';
+import { getUserID } from '../../../utils/Cryptic'
 
-const updateCreature = async (parent, { id, data }, { prisma, request }, info) => {
-    const creatureExists = prisma.exists.Creature({
-        id
-    })
+const updateCreature = async (
+  parent,
+  { id, data },
+  { prisma, request },
+  info
+) => {
+  const creatureExists = prisma.exists.Creature({
+    id
+  })
 
-    await getUserID(request)    //will throw an error if user is not authenticated
+  await getUserID(request) //will throw an error if user is not authenticated
 
-    if(!creatureExists)
-        throw new Error('Unable to find Creature')
+  if (!creatureExists) throw new Error('Unable to find Creature')
 
-    const updatedCreature = await prisma.mutation.updateCreature({
-        data: {
-            ...data,
-            attributes:{
-                update:{
-                    ...data.attributes
-                }
-            },
-            Conditions: {
-                update: {
-                    ...data.Conditions
-                }
-            }
+  const updatedCreature = await prisma.mutation.updateCreature(
+    {
+      data: {
+        ...data,
+        attributes: {
+          update: {
+            ...data.attributes
+          }
         },
-        where: {
-            id
+        Conditions: {
+          update: {
+            physical: {
+              update: {
+                fitness: {
+                  update: {
+                    ...data.Conditions.fitness
+                  }
+                }
+              }
+            },
+            spiritual: {
+              update: {
+                corruption: {
+                  update: {
+                    ...data.Conditions.corruption
+                  }
+                }
+              }
+            }
+          }
         }
-    }, info)
+      },
+      where: {
+        id
+      }
+    },
+    info
+  )
 
-    return updatedCreature
+  return updatedCreature
 }
 
 export default updateCreature
